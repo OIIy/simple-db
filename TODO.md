e2ecbbeb41baf3bb98e672a646e466abf37431c4
Olly McGilloway <ollymcgilloway@gmail.com>
2026-03-01 23:04:05 +0000

    Begin command parser

--- TODO.md
+++ TODO.md
@@ -5,3 +5,52 @@
 - [ ] Add stricter definitions for rows, they should be constrained 
 by column types
 - [ ] Add value indexing
+
+### Objective
+Create a lightweight datastore capable of
+
+- 100 writes/second
+- 200 reads/second
+- <10ms response times
+
+Data will be non-relational and stored in a single table.
+Data will be streamed and sent through a TCP listener.
+Operation results are then returned to the client.
+
+### Use Case Scenarios
+1. I want to send instructions to my database to modify my store.
+2. When I ask to read data I should get an object back containing all
+the columns and matching records that I've asked for.
+3. When I create/update/delete data, I should get a message back indicating 
+how many records were updated.
+4. I should be able to perform operations on my table simultaneously.
+
+### Scenario Breakdown
+1. The DB will need a way of parsing instructions.
+2. Reads should have parameters for filtering columns and rows.
+3. Each operation should have a corresponding response type.
+4. Operations should be non-blocking.
+
+### Design
+- Q. How should we interpret instructions such as create/read/update?
+- Q. How do we handle non-blocking IO?
+- Q. How do we store/format table data?
+
+#### How should we interpret instructions such as create/read/update?
+Instructions should be sent in a format similar to SQL.
+- **GET**: 'IN table_name GET [columns] WHERE [expression]',
+- **SET**: 'IN table_name SET [columns], ([values]) WHERE [expression]',
+
+We will need a simple parsing pipeline for this.
+- The instruction comes in over TCP. 
+- The bytes are fed to the pipeline.
+- The pipeline first splits the instruction into its parts. (Tokenization)
+- ["IN", "table_name", "GET", "col_1", "WHERE", "col_1", "=", "hello world!"]
+- Then the tokens are semantically parsed and converted to rust types.
+
+#### How do we handle non-blocking IO?
+To begin with, we will simply lock the table while it is being
+performing some operation.
+
+#### How do we store/format table data?
+We'll just start with a hashmap and serialize to JSON to begin with.
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 mod error;
 mod models;
 
+use std::collections::HashMap;
 use std::fs;
 use std::io::Read;
+use std::iter::Peekable;
 use std::net::{TcpListener, TcpStream};
-use crate::error::Result;
+use std::str::SplitWhitespace;
+use std::sync::{Mutex, OnceLock};
+use crate::error::{Result};
 use crate::models::column::Column;
 use crate::models::table::Table;
 
+static TABLE_STORE: OnceLock<Mutex<HashMap<String, Table>>> = OnceLock::new();
+
+fn get_table_store() -> &'static Mutex<HashMap<String, String>> {
+    TABLE_STORE.get_or_init()
+}
+
+struct TableStore {
+    tables: HashMap<String, Table>,
+}
+
+impl TableStore {
+    fn new() -> TableStore {
+        TableStore {
+            tables: HashMap::new()
+        }
+    }
+
+    fn add(&mut self, table_name: &str, table: Table) {
+        self.tables.insert(table_name.to_string(), table);
+    }
+
+    fn get(&self, table_name: String) -> Option<&Table> {
+        self.tables.get(&table_name)
+    }
+}
+
 fn main() -> Result<()> {
-    let table = seed_table()?;
+    let table = seed_table("simple_db")?;
 
     let json = serde_json::to_string(&table)?;
 
@@ -27,13 +57,87 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug)]
+enum Command {
+    Get {
+        table_name: String,
+        columns: Vec<String>,
+        where_clause: Vec<String>,
+    }
+}
+
+fn handle_command(buf: &[u8]) -> Result<String> {
+    /* Tokenization */
+    let command_text = String::from_utf8_lossy(buf);
+    let mut tokens = command_text.split_whitespace().into_iter().peekable();
+
+    /* Parsing */
+    let command = parse_command(&mut tokens)?;
+
+    let response = execute_command(&command)?;
+
+    Ok(response)
+}
+
+fn parse_command(tokens: &mut Peekable<SplitWhitespace>) -> Result<Command> {
+    // Ensure that the first token is "IN"
+    match tokens.next() {
+        Some("IN") => {},
+        Some(other) => return Err(format!("Syntax Error: Expected 'IN', found '{}'", other).into()),
+        None => return Err("Syntax Error: Empty command".into()),
+    }
+
+    let table_name = tokens.next().ok_or("Syntax Error: Expected table name after IN")?.to_string();
+
+    let action = tokens.next().ok_or("Syntax Error: Expected action after table name")?;
+
+    match action {
+        "GET" => {
+            let mut columns = Vec::new();
+
+            while let Some(&next_tok) = tokens.peek() {
+                if next_tok == "WHERE" {
+                    break;
+                }
+
+                columns.push(tokens.next().unwrap().to_string());
+            };
+
+            let mut where_clause = Vec::new();
+            if tokens.next() == Some("WHERE") {
+                where_clause = tokens.map(|x| x.to_string()).collect();
+            }
+
+            Ok(Command::Get {
+                table_name,
+                columns,
+                where_clause
+            })
+        }
+        _ => Err("Syntax Error: Empty command".into())
+    }
+}
+
+fn execute_command(command: &Command) -> Result<String> {
+    // Build the response string using the command properties
+
+    match command {
+        Command::Get => {
+            // Access table
+
+
+            Ok("Success".into())
+        }
+        _ => Err("Command Not Found".into())
+    }
+}
+
 fn handle_client(mut stream: TcpStream) -> Result<()> {
     let mut stream_buffer = [0; 1024];
 
     match stream.read(&mut stream_buffer) {
         Ok(bytes_read) if bytes_read > 0 => {
-            let received_text = String::from_utf8_lossy(&stream_buffer[..bytes_read]);
-            println!("{}", received_text);
+            handle_command(&stream_buffer[..bytes_read])?;
         },
         Ok(_) => println!("Client disconnected cleanly."),
         Err(e) => eprintln!("Failed to read from stream: {}", e),
@@ -42,8 +146,8 @@ fn handle_client(mut stream: TcpStream) -> Result<()> {
     Ok(())
 }
 
-fn seed_table() -> Result<Table> {
-    let mut table = Table::new("simple_db".to_owned());
+fn seed_table(table_name: &str) -> Result<Table> {
+    let mut table = Table::new(table_name.to_owned());
     let mut columns = Vec::new();
     columns.push(Column::new("Hello".to_string()));
     columns.push(Column::new("World".to_string()));
